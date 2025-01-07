c558d49f11a4fe90590d04207fe36368a5fdf078
abhay-chandra22 <abhaychandra2004@gmail.com>
2025-01-07 17:21:36 +0530

    Adding javascript property for slider functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const img = document.querySelectorAll('.header-slider ul img');
+const prev_but = document.querySelector('.control_prev');
+const next_but = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlider(){
+    for(let i = 0 ; i< img.length ; i++){
+        img[i].style.display = 'none';
+    }
+    img[n].style.display = 'block';
+}
+
+changeSlider();
+
+prev_but.addEventListener('click' , (e)=>{
+    if(n > 0){
+        n--;
+    }else{
+        n = img.length - 1 ;
+    }
+    changeSlider();
+})
+
+next_but.addEventListener('click' , (e)=>{
+    if(n < img.length-1){
+        n++;
+    }else{
+        n = 0 ;
+    }
+    changeSlider();
+})
